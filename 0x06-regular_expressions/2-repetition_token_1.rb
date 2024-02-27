4974fe4ef4abfd8f9d91913095ce82f20d98212a
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-02-27 16:34:16 +0200

    try 2

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Retrieve the argument passed to the script
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /hb?tn/
+
+# Scan the input string for matches using the defined pattern
+matches = input_string.scan(pattern)
+
+# Join the matched substrings into a single string
+result = matches.join
+
+# Output the resulting string
+puts result
