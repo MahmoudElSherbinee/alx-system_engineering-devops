0b55dc0d95c2e76ff66175d5a0953951b4131b56
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-04-19 21:08:48 +0200

    try 11

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# installs the package flask
-package { 'flask':
+#!/usr/bin/pup
+# Install flask from pip3.
+
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
