3abc3dd176eac62fc763251700c1cff2f8a05c38
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-04-19 21:34:32 +0200

    w

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 #!/usr/bin/pup
 # install a specific version of Flask (2.1.0)
+package { 'Werkzeu':
+  ensure   => '2.0.0',
+  provider => 'pip3',
+}
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
 }
