8fdcdd3f36b46026f6483005857c7c37266be0f8
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-04-19 21:13:41 +0200

    aa

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-# This Puppet file Install Flask on servers
-
-#service { 'pip install Flask':
-#    ensure  => 2.1.0
-#}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-exec { 'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    creates => '/usr/local/lib/python3.8/dist-packages/flask',
+# installs Flask version 2.1.0
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
